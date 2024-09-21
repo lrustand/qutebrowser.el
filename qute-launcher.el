364d4f858348206a8ec475aa811d51a58208571e
Lars Rustand <rustand.lars@gmail.com>
2024-09-21 16:36:21 +0200

    Rewrite using consult

--- qute-launcher.el
+++ qute-launcher.el
@@ -30,64 +30,112 @@
 ;; i.e. remove Google, Ebay, Amazon, Youtube searches etc
 (require 'sqlite)
 (require 'marginalia)
-(defun qute-launcher--select-from-history (url)
-  (let* ((db (sqlite-open "~/.local/share/qutebrowser/history.sqlite"))
-         (history (sqlite-select db "SELECT url,substr(title,0,99) FROM History GROUP BY url ORDER BY COUNT(url) DESC"))
-         (candidates (mapcar (lambda (row)
-                               (let* ((url (nth 0 row))
-                                      (title (nth 1 row))
-                                      (display-url (truncate-string-to-width url 50 0 ?\ )))
-                                 (cons
-                                  (format "%s %s"
-                                          display-url
-                                          (propertize title
-                                                      'face 'marginalia-value))
-                                  url)))
-                             history))
-         (completion-table
-          (lambda (string pred action)
-            (if (eq action 'metadata)
-                `(metadata
-                  (display-sort-function . identity)
-                  (cycle-sort-function . identity)
-                  (category . qutebrowser-history)
-                  (annotation-function . nil))
-              (complete-with-action action candidates string pred))))
-         (selected (completing-read "Choose URL from history: " completion-table nil nil url)))
-    ;; Return the URL of the selected candidate
-    (if selected
-        (cdr (or (assoc selected candidates)
-                 (cons nil selected)))
-      nil)))
+(require 'consult)
+(require 'exwm)
 
-(defun qute-launcher--internal (&optional url target prefilled)
-  (let ((url (or (and (not prefilled) url)
-                 (qute-launcher--select-from-history url)))
-        (pipe (getenv "QUTE_FIFO"))
-        (flag (pcase target
-                    ('window "-w")
-                    ('tab "-t")
-                    ('private-window "-p")
-                    ('auto ""))))
+(defvar qutebrowser--target 'auto)
+
+(defun qutebrowser-history ()
+  (let ((db (sqlite-open "~/.local/share/qutebrowser/history.sqlite")))
+    (sqlite-select db "SELECT url,substr(title,0,99) FROM History GROUP BY url ORDER BY
+COUNT(url) DESC")))
+
+(defun qutebrowser-history-candidates ()
+  "Returns a list of completion candidates from qutebrowser
+history. Candidates contain the url, and a pseudo-annotation with the
+website title, to allow searching based on either one."
+  (let* ((history (qutebrowser-history)))
+    (mapcar (lambda (row)
+              (let* ((url (nth 0 row))
+                     (title (nth 1 row))
+                     (display-url (truncate-string-to-width url 50 0 ?\ )))
+                (cons
+                 (format "%s %s"
+                         display-url
+                         (propertize title
+                                     'face 'marginalia-value))
+                 url)))
+            history)))
+
+(defun qutebrowser-open-url (url)
+  (let* ((pipe (getenv "QUTE_FIFO"))
+         (target (or qutebrowser--target 'auto))
+         (flag (pcase target
+                 ('window "-w")
+                 ('tab "-t")
+                 ('private-window "-p")
+                 ('auto ""))))
     (if pipe
         (write-region (format "open %s %s" flag url) nil pipe t)
       (start-process "qutebrowser" nil "qutebrowser" "--target" (symbol-name target) url))))
 
+(defun qute-launcher--internal (&optional url prefilled)
+  (if (and url (not prefilled))
+      (qutebrowser-open-url url)
+    (let* ((res (consult--multi '(qutebrowser-buffer-source
+                                  qutebrowser-history-buffer-source)
+                                :initial url
+                                :sort nil))
+           (plist (cdr res))
+           (selected (car res)))
+      ;; If none of the buffer sources handled it
+      (unless (plist-get plist :match)
+        (qutebrowser-open-url selected)))))
+
 (defun qute-launcher (&optional url _ prefilled)
   (interactive)
-  (qute-launcher--internal url 'auto prefilled))
+  (qute-launcher--internal url prefilled))
 
 (defun qute-launcher-tab (&optional url _ prefilled)
   (interactive)
-  (qute-launcher--internal url 'tab prefilled))
+  (let ((qutebrowser--target 'tab))
+    (qute-launcher--internal url prefilled)))
 
 (defun qute-launcher-window (&optional url _ prefilled)
   (interactive)
-  (qute-launcher--internal url 'window prefilled))
+  (let ((qutebrowser--target 'window))
+    (qute-launcher--internal url prefilled)))
 
 (defun qute-launcher-private (&optional url _ prefilled)
   (interactive)
-  (qute-launcher--internal url 'private-window prefilled))
+  (let ((qutebrowser--target 'private-window))
+    (qute-launcher--internal url prefilled)))
+
+
+(defvar qutebrowser-buffer-source
+  `(;; consult-buffer source for open Qutebrowser windows
+    :name "Qutebrowser buffers"
+    :hidden nil
+    :narrow ?q
+    :category buffer
+    ;; Specify either :action or :state
+    :action ,#'consult--buffer-action ;; No preview
+    ;; :state ,#'consult--buffer-state  ;; Preview
+    ;; HACK to workaround that it doesn't work in the other source
+    :items
+    ,(lambda () (consult--buffer-query
+                 :sort 'visibility
+                 :as #'buffer-name
+                 :predicate (lambda (buf)
+                              (with-current-buffer buf
+                                (string-equal "qutebrowser"
+                                              exwm-class-name)))
+                 :mode 'exwm-mode))))
+
+
+(defvar qutebrowser-history-buffer-source
+  `(;; consult-buffer source for Qutebrowser history
+    :name "Qutebrowser history"
+    :hidden nil
+    :narrow ?h
+    :history nil
+    :category buffer
+    :action ,#'qutebrowser-open-url
+    :items
+    ,#'qutebrowser-history-candidates))
+
+;;(add-to-list 'consult-buffer-sources 'qutebrowser-buffer-source 'append)
+
 
 
 (provide 'qute-launcher)
