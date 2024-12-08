ea2d81e1d93ccf2caf8e3a5c6bb2c8e18f417339
Lars Rustand <rustand.lars@gmail.com>
2024-12-08 12:50:44 +0100

    Improve repl

--- repl.el
+++ repl.el
@@ -1,42 +1,36 @@
+(defvar qutebrowser-repl-prompt ">>> ")
 
-(require 'comint)
+(defun qutebrowser-create-repl-buffer ()
+  (get-buffer-create "*Qutebrowser REPL*"))
 
-(defun make-python (buffer)
-  (make-network-process
-   :name "python-repl"
-   :family 'local
-   :service  "/tmp/emacs-ipc"
-   :buffer buffer
-   :filter 'comint-output-filter
-   :sentinel 'python-repl-sentinel))
-
-(defun python-repl-start ()
-  "Start the Python REPL using comint."
+(defun qutebrowser-repl-send-input ()
   (interactive)
-  (let ((buffer (get-buffer-create "*Python REPL*")))
-    (with-current-buffer buffer
-      (setq-local comint-input-sender
-                  (lambda (proc string)
-                    (comint-simple-send proc (json-encode `((cmd . "eval")
-                                                            (args . ,string))))))
-      (setq keepalive-timer
-            (run-at-time 0 1
-                         (lambda ()
-                           (let ((proc (get-process "python-repl")))
-                             (comint-simple-send proc (json-encode `((cmd . "keepalive")
-                                                                     (args . "nothing"))))))))
-      (unless (comint-check-proc buffer)
-        (let ((process (make-python buffer)))
-          (comint-mode))))))
+  (let ((input (buffer-substring-no-properties
+                (+ (line-beginning-position)
+                   (1- (length qutebrowser-repl-prompt)))
+                (point-max))))
+    (qutebrowser-rpc-call `((eval . ,input)))
+    (insert "\n")))
 
-(defun python-repl-sentinel (process event)
-  (message "EVENT!!! %s" event)
-  (if (or (string-match "closed" event)
-          (string-match "broken" event))
-      (progn
-        (message "Connection lost, attempting to reconnect...")
-                       (let ((buffer (process-buffer process)))
-                         (when buffer
-                           ;; Restart the process and associate it with the buffer
-                           (with-current-buffer buffer
-                             (python-repl-start)))))))
+(defun qutebrowser-repl-receive-response (response)
+  (with-current-buffer (qutebrowser-create-repl-buffer)
+    (goto-char (point-max))
+    (insert response "\n")
+    (insert qutebrowser-repl-prompt)))
+
+(define-derived-mode qutebrowser-repl-mode fundamental-mode "Qutebrowser REPL"
+  "Major mode for Qutebrowser REPL."
+  (use-local-map qutebrowser-repl-mode-map))
+
+(defvar qutebrowser-repl-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "RET") 'qutebrowser-repl-send-input)
+    map))
+
+(defun qutebrowser-start-repl ()
+  (interactive)
+  (let ((repl-buffer (qutebrowser-create-repl-buffer)))
+    (with-current-buffer repl-buffer
+      (qutebrowser-repl-mode)
+      (insert qutebrowser-repl-prompt))
+    (switch-to-buffer-other-window repl-buffer)))
