60202bb0ef8ee26782c5035f4da9b636d1cf54db
Isaac Haller <isaac@hllr.xyz>
2025-01-22 13:24:00 -0600

    Remove consult-based launcher commands, make URL selection function customizable

--- qutebrowser-consult.el
+++ qutebrowser-consult.el
@@ -23,6 +23,9 @@
 ;; added as additional sources to 'consult-buffer' or similar. See
 ;; 'consult-buffer-sources' and 'consult--multi'.
 
+;; To use Consult for all Qutebrowser-related selection, set
+;; `qutebrowser-selection-function' to `qutebrowser-consult-select-url'.
+
 ;;; Change Log:
 
 ;;; Code:
@@ -67,7 +70,8 @@
         :category 'bookmark
         :face 'consult-bookmark
         :action #'qutebrowser-bookmark-jump
-        :items #'qutebrowser-bookmarks-list)
+        :items #'qutebrowser-bookmarks-list
+	:qb-tofu ?m)
   "Consult source for Qutebrowser bookmarks.")
 
 ;;;; Command source
@@ -81,7 +85,8 @@
 	:category 'other
 	:action #'qutebrowser-send-commands
         :annotate #'qutebrowser-consult--annotate
-	:items (apply-partially #'qutebrowser-command-search '(":")))
+	:items (apply-partially #'qutebrowser-command-search '(":"))
+	:qb-tofu ?c)
   "Consult source for Qutebrowser commands.")
 
 ;;;###autoload
@@ -134,87 +139,27 @@ Set initial completion input to INITIAL."
   (let ((new-source (seq-copy source)))
     (plist-put new-source :action nil)))
 
-;; NOTE does this still need to exist?
+;;;###autoload
 (defun qutebrowser-consult-select-url (&optional initial default)
-  "Dynamically select a URL, buffer, or command using consult.
-INITIAL sets the initial input in the minibuffer."
-  (let ((consult-async-min-input 0)
-        (consult-async-split-style nil)
-	(sources (list qutebrowser-consult--exwm-buffer-source
-		       qutebrowser-consult--bookmark-source
-		       qutebrowser-consult--history-source
-		       qutebrowser-consult--command-source)))
-    (consult--multi (mapcar #'qutebrowser-consult--suppress-action sources)
-		    :prompt (if default
-				(format "Select (default %s): " default)
-			      "Select: ")
-		    :default default
-		    :sort nil
-		    :initial initial
-		    :require-match nil)))
-
-;;;###autoload
-(defun qutebrowser-consult-launcher (&optional initial target)
-  "Select a URL to open in Qutebrowser using Consult.
-
-Set initial completion input to INITIAL. Open the URL in TARGET or the
-default target if nil.
-
-Modify `qutebrowser-consult-launcher-sources' to change which sources
-are included."
-  (interactive)
-  (let* ((consult-async-split-style nil)
-	 (qutebrowser-default-open-target (or target qutebrowser-default-open-target))
-	 (selected (consult--multi qutebrowser-consult-launcher-sources
-				   :initial initial
-				   :sort nil)))
-    (unless (plist-get (cdr selected) :match)
-      (qutebrowser-open-url (car selected)))))
-
-;;;###autoload
-(defun qutebrowser-consult-launcher-tab (&optional initial)
-  "Select a URL to open in a new tab using Consult.
-
-Set initial completion input to INITIAL.
-Modify `qutebrowser-consult-launcher-sources' to change which sources
-are included."
-  (interactive)
-  (qutebrowser-consult-launcher initial 'tab))
-
-;;;###autoload
-(defun qutebrowser-consult-launcher-window (&optional initial)
-  "Select a URL to open in a new window using Consult.
-
-Set initial completion input to INITIAL.
-Modify `qutebrowser-consult-launcher-sources' to change which sources
-are included."
-  (interactive)
-  (qutebrowser-consult-launcher initial 'window))
-
-;;;###autoload
-(defun qutebrowser-consult-launcher-private (&optional initial)
-  "Select a URL to open in a private window using Consult.
-
-Set initial completion input to INITIAL.
-Modify `qutebrowser-consult-launcher-sources' to change which sources
-are included."
-  (interactive)
-  (qutebrowser-consult-launcher initial 'private-window))
-
-(define-minor-mode qutebrowser-consult-mode
-  "Use Consult for Qutebrowser completion."
-  :lighter nil
-  :global t
-  (if qutebrowser-consult-mode
-      (progn
-	(advice-add 'qutebrowser-launcher :override #'qutebrowser-consult-launcher)
-	(advice-add 'qutebrowser-launcher-tab :override #'qutebrowser-consult-launcher-tab)
-	(advice-add 'qutebrowser-launcher-window :override #'qutebrowser-consult-launcher-window)
-	(advice-add 'qutebrowser-launcher-private :override #'qutebrowser-consult-launcher-private))
-    (advice-remove 'qutebrowser-launcher #'qutebrowser-consult-launcher)
-    (advice-remove 'qutebrowser-launcher-tab #'qutebrowser-consult-launcher-tab)
-    (advice-remove 'qutebrowser-launcher-window #'qutebrowser-consult-launcher-window)
-    (advice-remove 'qutebrowser-launcher-private #'qutebrowser-consult-launcher-private)))
+  "Backend for `qutebrowser-select-url' based on Consult."
+  (let* ((consult-async-min-input 0)
+         (consult-async-split-style nil)
+	 (sources (list qutebrowser-consult--exwm-buffer-source
+			qutebrowser-consult--bookmark-source
+			qutebrowser-consult--history-source
+			qutebrowser-consult--command-source))
+	 (selection (consult--multi
+		     (mapcar #'qutebrowser-consult--suppress-action sources)
+		     :prompt (if default
+				 (format "Select (default %s): " default)
+			       "Select: ")
+		     :default default
+		     :sort nil
+		     :initial initial
+		     :require-match nil)))
+    ;; Because `qutebrowser' still needs tofus for dispatching, we have to add them here.
+    (qutebrowser--tofu-append (car selection)
+			      (plist-get (cdr selection) :qb-tofu))))
 
 (provide 'qutebrowser-consult)
 
